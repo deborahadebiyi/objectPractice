3e6f0dbc0feb1b909dfecd6324a4200e86f4e2db
deborahadebiyi <16781923+deborahadebiyi@users.noreply.github.com>
2021-03-15 12:40:46 +0000

    Defining EvilCircle's methods

--- main.js
+++ main.js
@@ -36,6 +36,63 @@ function EvilCircle(x, y, velX, velY, exists, color, size) {
   this.size = 10;
 }
 
+EvilCircle.prototype.draw = function() {
+  ctx.beginPath();
+  lineWidth = 3;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function() {
+  if ((this.x + this.size) >= width) {
+    this.x = -(this.size);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.x = +(this.size);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.y = -(this.size);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.y = +(this.size);
+  }
+}
+
+EvilCircle.prototype.setControls = function() {
+  let _this = this;
+  window.onkeydown = function(e) {
+    if (e.key === 'a') {
+      _this.x -= _this.velX;
+    } else if (e.key === 'd') {
+      _this.x += _this.velX;
+    } else if (e.key === 'w') {
+      _this.y -= _this.velY;
+    } else if (e.key === 's') {
+      _this.y += _this.velY;
+    }
+  }
+}
+
+
+  EvilCircle.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+      if (balls[j].exists) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].exists = false;
+        }
+      }
+    }
+  }
+  
+
 Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
@@ -108,6 +165,8 @@ function loop() {
   }
 
   requestAnimationFrame(loop);
+
+  let evilObj = new EvilCircle(x, y, 20, 20, true, 'white', 50);
 }
 
 loop();
